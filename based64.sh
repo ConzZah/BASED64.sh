4db0f6acfb686addc6e14c7b2e5def4959955ee4
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-03-29 21:17:39 +0100

    Updated based64.sh to [v0.1x]

--- based64.sh
+++ based64.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
-# ============================================
-# based64 [en/decoder] v0.1 | Author: Conzzah
-# ============================================
+# =============================================
+# based64 [en/decoder] v0.1x | Author: Conzzah
+# =============================================
 clear; cd /; cd /home/$USER/Documents  #<-- standard savepath for files
-# begin setting functions
+# /// main functions start
 function logo {
-echo "LAST CHANGE TO CODE @ [ 26.03.2024 / 02:42 ]"
-echo "==============================================="
-echo "[ ~~~ BASE(D) 64 ENCODER / DECODER [v0.1] ~~~ ]"
-echo "==============================================="
+#echo "LAST CHANGE TO CODE @ [ 29.03.2024 / 21:01 ]"
+echo "================================================"
+echo "[ ~~~ BASE(D) 64 ENCODER / DECODER [v0.1x] ~~~ ]"
+echo "================================================"
 echo ""
 }
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# menu stage 1 start (Main Menu)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function main_menu {
+clear; logo
+echo "/ TEXT or FILE ?" # lets user choose between encoding/decoding either text or file
+echo "~~~~~~~~~~~~~~~~~"
+select tf in "text" "file"; do
+	case $tf in 
+	"text" ) text-menu; continue;;
+	"file" ) file-menu; continue;; 
+	esac
+done
+}
+#~~~~~~~~~~~~~~~~~~~~
+# menu stage 1 end
+#~~~~~~~~~~~~~~~~~~~~
 # menu stage 2 start
 #~~~~~~~~~~~~~~~~~~~~
 #text-menu
-function _text-menu {
+function text-menu {
+clear; logo
+echo "/ ENCODE or DECODE TEXT ?"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
 select ed in "encode text" "decode text"; do
 	case $ed in 
 	"encode text" ) encode64_text; break;; 
@@ -23,9 +42,11 @@ select ed in "encode text" "decode text"; do
 	esac
 done
 }
-
 #file-menu
-function _file-menu {
+function file-menu {
+clear; logo
+echo "/ ENCODE or DECODE FILE ?"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
 select ed in "encode file" "decode file"; do
 	case $ed in 
 	"encode file" ) encode64_file; break;; 
@@ -33,83 +54,77 @@ select ed in "encode file" "decode file"; do
 	esac
 done
 }
-
+#~~~~~~~~~~~~~~~~~~~~
 # menu stage 2 end
 #~~~~~~~~~~~~~~~~~~~~
 #main functions start
 #~~~~~~~~~~~~~~~~~~~~
 #text_encode
 function encode64_text {
-clear
-logo; echo ""
+clear; logo
 echo "// ENCODE TEXT"
 echo "~~~~~~~~~~~~~~~~~"
 echo "paste or type text to encode:"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"; echo ""
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"; echo ""
 read encode_text
 echo ""
 echo "RESULT:"
-echo "~~~~~~~"
+echo "~~~~~~~~"
 echo $encode_text | base64;
 echo ""
+return2main_menu
 }
-
 #text_decode
 function decode64_text {
-clear
-logo; echo ""
+clear; logo
 echo "// DECODE TEXT"
 echo "~~~~~~~~~~~~~~~~~"
 echo "paste or type text to decode:"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"; echo ""
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"; echo ""
 read decode_text
 echo ""
 echo "RESULT:"
-echo "~~~~~~~"
+echo "~~~~~~~~"
 echo $decode_text | base64 -d
 echo ""
+return2main_menu
 }
-
 #file_encode
 function encode64_file {
-clear
-logo; echo ""
+clear; logo
 echo "// ENCODE FILE"
 echo "~~~~~~~~~~~~~~~"
-echo "paste path to file:"; echo ""
+echo "paste path to file:"
+echo "~~~~~~~~~~~~~~~~~~~~~"; echo
 read encode_file
-echo "choose name for output file ( format set to .txt by default )"
+echo "choose name for output file ( extension will be .txt by default )"; echo ""
 read output_filename
 base64 $encode_file > encoded64_$output_filename.txt
-echo ""; echo "file saved as: encoded64_$output_filename.txt"
+echo "file saved as: encoded64_$output_filename.txt"
 unset $output_filename
+return2main_menu
 }
-
 #file_decode
 function decode64_file {
-clear
-logo; echo ""
+clear; logo
 echo "// DECODE FILE"
 echo "~~~~~~~~~~~~~~~"
-echo "paste path to file:"; echo ""
+echo "paste path to file:"
+echo "~~~~~~~~~~~~~~~~~~~~~"; echo
 read decode_file
-echo "input filename & filetype ( any name & format of the original file )"; echo "" 
+echo "input filename & filetype ( name & format of the original file )"; echo "" 
 read output_filename_filetype
 base64 -d $decode_file > $output_filename_filetype
 echo "file saved as: $output_filename_filetype"
 unset $output_filename_filetype
+return2main_menu
 }
-
-# main functions end
-#~~~~~~~~~~~~~~~~~~~~
-# menu stage 1 start
-#~~~~~~~~~~~~~~~~~~~~
-# lets user choose between encoding/decoding either text or file
-logo; echo "ENCODE / DECODE TEXT OR FILE?"; echo ""
-select sf in "text" "file"; do
-	case $sf in 
-	"text" ) _text-menu; continue;;
-	"file" ) _file-menu; continue;; 
-	esac
-done
-#menu stage 1 end
+# return2main_menu
+function return2main_menu {
+echo "[ ~~~ PRESS ENTER FOR MAIN MENU ~~~ ]"
+read -n 1 -s
+main_menu
+}
+# main functions end \\\
+# calls main_menu at the end to ensure all functions are set.
+main_menu
